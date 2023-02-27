788698691babfe1011d5f9c210fd536cee5f77a3
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-02-27 13:02:35 +0900

    visual update

--- components/main/Ghibli_visual.js
+++ components/main/Ghibli_visual.js
@@ -1,4 +1,6 @@
+import styles from "@/styles/main/Ghibli_visual.module.scss";
+
 const Ghibli_visual = () => {
-  return <section>Ghibli_visual</section>;
+  return <section className={styles.container}>Ghibli_visual</section>;
 };
 export default Ghibli_visual;
