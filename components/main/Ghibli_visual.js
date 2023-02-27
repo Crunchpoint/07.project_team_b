023ab2ec15c12d43bc05f0079303846535a1ac8c
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-02-27 12:55:41 +0900

    ghibli visual update

---
+++ components/main/Ghibli_visual.js
@@ -0,0 +1,4 @@
+const Ghibli_visual = () => {
+  return <div>Ghibli_visual</div>;
+};
+export default Ghibli_visual;
--- pages/main/ghibli.js
+++ pages/main/ghibli.js
@@ -1,10 +1,11 @@
 import Meta from "@/components/Meta";
+import Ghibli_visual from "@/components/main/Ghibli_visual";
 
 const ghibli = () => {
   return (
     <>
       <Meta title='title' name='name' description='description' />
-      <div>ghibli</div>
+      <Ghibli_visual />
     </>
   );
 };
