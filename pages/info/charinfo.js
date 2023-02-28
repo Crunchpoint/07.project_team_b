9b2774133b08cb827a49fc2b975de17ce65567e5
PronCan <twojini98@gmail.com>
2023-02-28 09:52:14 +0900

    create /charinfo

---
+++ pages/info/charinfo.js
@@ -0,0 +1,13 @@
+import Meta from "@/components/Meta";
+import CharInfo from "@/components/info/CharInfo";
+
+const charinfo = () => {
+  return (
+    <>
+      <Meta title='CharInfo' name='name' description='description' />
+      <CharInfo />
+    </>
+  )
+}
+
+export default charinfo
