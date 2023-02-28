16bb0e560d0f6c2f421f1ca2f53a90328b47f15c
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-02-28 14:14:16 +0900

    visual update

--- components/main/Ghibli_visual.js
+++ components/main/Ghibli_visual.js
@@ -5,14 +5,32 @@ import { MyContext } from "@/components/context/Context";
 
 const Ghibli_visual = () => {
   const { data } = useContext(MyContext);
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("GHIBLI");
   console.log(data);
+
+  const textEffect = () => {};
 
   return (
     data.length > 0 && (
       <section className={styles.container}>
         <div className={styles.inner}>
-          <h1>{title}</h1>
+          <div>
+            <h1 data-title={title}>{title}</h1>
+          </div>
+          <svg id='filters' style={{ position: "absolute", zIndex: "-1" }}>
+            <defs>
+              <filter id='threshold'>
+                <feColorMatrix
+                  in='SourceGraphic'
+                  type='matrix'
+                  values='1 0 0 0 0
+									0 1 0 0 0
+									0 0 1 0 0
+									0 0 0 255 -140'
+                />
+              </filter>
+            </defs>
+          </svg>
           <nav>
             <ul>
               {data?.map((item, key) => {
@@ -20,12 +38,19 @@ const Ghibli_visual = () => {
                   <li key={key}>
                     <Link href=''>
                       <figure>
-                        <img src={item.src} alt='#' data-title={item.name} onMouseEnter={() => setTitle(item.name)} onMouseLeave={() => setTitle("")} />
+                        <img src={item.src} alt='#' onMouseEnter={() => setTitle(item.name)} onMouseLeave={() => setTitle("GHIBLI")} />
                       </figure>
                     </Link>
                   </li>
                 );
               })}
+              <li>
+                <Link href=''>
+                  <figure>
+                    <img src='../src/img/main/구름png.png' alt='#' />
+                  </figure>
+                </Link>
+              </li>
             </ul>
           </nav>
         </div>
