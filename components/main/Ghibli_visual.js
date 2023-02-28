affdd5577610f0892f46480cedb720788c06460c
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-02-28 11:05:48 +0900

    visual update

--- components/main/Ghibli_visual.js
+++ components/main/Ghibli_visual.js
@@ -1,23 +1,37 @@
 import styles from "@/styles/main/Ghibli_visual.module.scss";
 import Link from "next/link";
+import { useContext, useState } from "react";
+import { MyContext } from "@/components/context/Context";
 
 const Ghibli_visual = () => {
+  const { data } = useContext(MyContext);
+  const [title, setTitle] = useState("");
+  console.log(data);
+
   return (
-    <section className={styles.container}>
-      <div className={styles.inner}>
-        <nav>
-          <ul>
-            <li>
-              <Link href='/'>
-                <figure>
-                  <img src='' alt='#' />
-                </figure>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </section>
+    data.length > 0 && (
+      <section className={styles.container}>
+        <div className={styles.inner}>
+          <h1>{title}</h1>
+          <nav>
+            <ul>
+              {data?.map((item, key) => {
+                return (
+                  <li key={key}>
+                    <Link href=''>
+                      <figure>
+                        <img src={item.src} alt='#' data-title={item.name} onMouseEnter={() => setTitle(item.name)} onMouseLeave={() => setTitle("")} />
+                      </figure>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </section>
+    )
   );
 };
+
 export default Ghibli_visual;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,8 @@
 import Hello from "@/components/intro/Hello";
 import Loading from "@/components/intro/Loading";
 import Meta from "@/components/Meta";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { useContext } from "react";
-import { MyContext } from "@/components/context/Context";
 
 export default function Home() {
-  const { setLoadingProgress } = useContext(MyContext);
-  const router = useRouter();
-
-  useEffect(() => {
-    router.pathname === "/" && setLoadingProgress(0);
-  }, [router.pathname]);
-
   return (
     <>
       <Meta title='title' name='name' description='description' />
