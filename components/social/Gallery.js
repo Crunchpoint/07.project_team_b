a7e2e06cd7d180492dfc217968efc542610a9c6a
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-03-02 18:05:08 +0900

    feat: added social pages

--- components/social/Gallery.js
+++ components/social/Gallery.js
@@ -4,25 +4,25 @@ const Gallery = () => {
   return (
     <div className={styles.gallery}>
       <figure>
-        <img src='' alt='' />
+        <img src='../src/img/main/Boeun_png.png' alt='' />
       </figure>
       <figure>
-        <img src='' alt='' />
+        <img src='../src/img/main/Boeun_bg.jpg' alt='' />
       </figure>
       <figure>
-        <img src='' alt='' />
+        <img src='../src/img/main/Boeun_png2.png' alt='' />
       </figure>
       <figure>
-        <img src='' alt='' />
+        <img src='../src/img/main/howl_bg.jpg' alt='' />
       </figure>
       <figure>
-        <img src='' alt='' />
+        <img src='../src/img/main/howl_png.png' alt='' />
       </figure>
       <figure>
-        <img src='' alt='' />
+        <img src='../src/img/main/howl_png2.png' alt='' />
       </figure>
       <figure>
-        <img src='' alt='' />
+        <img src='../src/img/main/Boeun_bg.jpg' alt='' />
       </figure>
     </div>
   );
