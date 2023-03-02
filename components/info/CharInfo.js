920dea598cd6858d3de28ca648dec2f74433b8a4
PronCan <twojini98@gmail.com>
2023-03-02 21:52:55 +0900

    Update CharInfo.js

--- components/info/CharInfo.js
+++ components/info/CharInfo.js
@@ -11,15 +11,12 @@ import styles from "@/styles/info/CharInfo.module.scss";
 import { useRouter } from "next/router";
 
 const CharInfo = () => {
-<<<<<<< Updated upstream
-=======
   const router = useRouter;
   const {data2, setData2} = useContext(MyContext);
   const ani_name = 'laputa';// 임시 이름
   const _data = data2.laputa
   console.log(_data);
 
->>>>>>> Stashed changes
   return (
     <div className={styles.app_info}>
       <div className={styles.wrap_app}>
@@ -46,23 +43,6 @@ const CharInfo = () => {
           // pagination={true}
           modules={[EffectCoverflow, Pagination, Scrollbar]}
           className={`${styles.body}, ${styles.info_slider}`}>
-<<<<<<< Updated upstream
-            <SwiperSlide className={`${styles.swiper_slide}`}>
-              <div className={`${styles.swiper_slide_detail}`}
-                onClick={(obj, key)=> {
-                  // window.location.href=`/info/charinfo_detail/${key}`
-                  // 임시 페이지 이동
-                  window.location.href=`/info/charinfo_detail`
-                }}>
-                <img src="/src/img/info/ponyo/sosuke3.jpg"></img>
-                <p>소스케</p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className={`${styles.swiper_slide}`}>Slide 2</SwiperSlide>
-            <SwiperSlide className={`${styles.swiper_slide}`}>Slide 3</SwiperSlide>
-            <SwiperSlide className={`${styles.swiper_slide}`}>Slide 4</SwiperSlide>
-            <SwiperSlide className={`${styles.swiper_slide}`}>Slide 5</SwiperSlide>
-=======
             {
               _data&&_data.map((obj, key) => {
                 // console.log(obj);
@@ -80,7 +60,6 @@ const CharInfo = () => {
                 </SwiperSlide>
               })
             }
->>>>>>> Stashed changes
           </Swiper>
           </div>
         </div>
