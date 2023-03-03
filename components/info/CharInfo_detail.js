54e106cc09269058c11fe0226c6ed99d7c308d65
PronCan <twojini98@gmail.com>
2023-03-03 16:31:37 +0900

    fix: 값 없을때 오류현상 수정

--- components/info/CharInfo_detail.js
+++ components/info/CharInfo_detail.js
@@ -8,53 +8,49 @@ import Link from "next/link";
 const CharInfo_detail = () => {
   const router = useRouter();
   const name = router.query.name;
-  const str_data = router.query.data
-  let data='';
-  if(!str_data || !router.query.data) {
-    return
+  const str_data = router.query.data;
+  let data = "";
+  if (!str_data || !router.query.data) {
+    return;
   } else {
     data = JSON.parse(str_data);
-    console.log(data)
+    console.log(data);
   }
-  useEffect(()=> {
-    setTimeout(()=> {
-
-    }, 1000)
-  })
+  useEffect(() => {
+    setTimeout(() => {}, 1000);
+  });
   return (
     <>
-    <div className={styles.app}>
-    <div className={styles.wrap_back}>
-        <button onClick={()=> {
-          window.location.href = '/info/charinfo'
-        }}></button>
-    </div>
-    <div className={styles.wrap_logo}>
-        <button onClick={()=> {
-          window.location.href = '/info/charinfo'
-        }}></button>
-    </div>
-    <Logo className={styles.logo_float}></Logo>
-      <div className={`${styles.wrap_app} ${styles.wrap_app_detail}`}>
-        <div className={styles.detail_left}>
-          <img src="/src/img/info/ponyo/sosuke3.jpg"/>
-          <p>{data.name_eng}({data.name_jap})</p>
+      <div className={styles.app}>
+        <div className={styles.wrap_back}>
+          <button
+            onClick={() => {
+              window.location.href = "/info/charinfo";
+            }}
+          ></button>
         </div>
-        <div className={styles.detail_right}>
-          <p>
-            "{data.quotes}"
-          </p>
-          <p>
-          성우: {data.voice_eng}({data.voice_jap})
-          </p>
-          <p className={styles.detail_right_path}>
-            <Link href={data.url}>페이지 바로가기 ▶️</Link>
-          </p>
+        <Logo className={styles.logo_float}></Logo>
+        <div className={`${styles.wrap_app} ${styles.wrap_app_detail}`}>
+          <div className={styles.detail_left}>
+            {/* <img src="/src/img/info/ponyo/sosuke3.jpg"/> */}
+            <img src={data.src} />
+            <p>
+              {data.name_eng}({data.name_jap})
+            </p>
+          </div>
+          <div className={styles.detail_right}>
+            <p>{data.quotes == "" ? <span></span> : `"${data.quotes}"`}</p>
+            <p>
+              성우: {data.voice_eng}({data.voice_jap})
+            </p>
+            <p className={styles.detail_right_path}>
+              <Link href={data.url}>페이지 바로가기 ▶️</Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default CharInfo_detail
+export default CharInfo_detail;
