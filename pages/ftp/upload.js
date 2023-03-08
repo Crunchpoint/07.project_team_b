3f4c21f5f00045d8227a3f0c7a95bf9597c7f224
PronCan <twojini98@gmail.com>
2023-03-08 18:20:20 +0900

    파일 중간백업

---
+++ pages/ftp/upload.js
@@ -0,0 +1,13 @@
+import Meta from "@/components/Meta";
+import Upload from "@/components/ftp/Upload";
+
+const upload = () => {
+  return (
+    <>
+      <Meta title='upload' name='name' description='description' />
+      <Upload />
+    </>
+  )
+}
+
+export default upload
