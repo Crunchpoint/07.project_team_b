3f16f93d3a50b027416a9ef33f518e02fd6b6840
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-03-17 03:40:59 +0900

    feat: minor update

--- pages/info/charinfo_detail.js
+++ pages/info/charinfo_detail.js
@@ -3,12 +3,12 @@ import Detail from "@/components/info/Detail";
 import { useRouter } from "next/router";
 
 const charinfo_detail = () => {
-  // const router = useRouter();
-  // const { query } = router;
+  const router = useRouter();
+  const { query } = router;
 
   return (
     <>
-      {/* <Meta title={`${query.ani_name} - ${query.name}`} name='name' description='description' /> */}
+      <Meta title={`${query.ani_name} - ${query.name}`} name='name' description='description' />
       <Detail />
     </>
   );
