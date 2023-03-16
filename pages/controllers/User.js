cd9e277e6e9054bd0d6f8185ff8470c627a5fa00
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-03-17 03:48:14 +0900

    feat: bug fix

--- pages/controllers/User.js
+++ pages/controllers/User.js
@@ -1,32 +1,32 @@
-const express = require('express')
-  const router = express.Router();
-  const multer  = require('multer')
-  const upload = multer({
-      dest: 'uploads/'
-  })
-  const UserController = require('../../pages/controllers/User')
+// const express = require('express')
+//   const router = express.Router();
+//   const multer  = require('multer')
+//   const upload = multer({
+//       dest: 'uploads/'
+//   })
+//   const UserController = require('../../pages/controllers/User')
 
-  router.post('/api/file_upload', upload.single('image'), UserController.uploadProfile)
+//   router.post('/api/file_upload', upload.single('image'), UserController.uploadProfile)
 
-  // app.post('/upload', upload.array(), (req, res, next) => {
-  //     console.log('req.body', req.body);
-  // })
+//   // app.post('/upload', upload.array(), (req, res, next) => {
+//   //     console.log('req.body', req.body);
+//   // })
 
-  // app.listen('3000', ()=> {
-  //     console.log('active');
-  // })
+//   // app.listen('3000', ()=> {
+//   //     console.log('active');
+//   // })
 
-  module.exports = router;
-  
-const User = {
-  uploadProfile: async (req, res) => {
-    const img = req.file.path;
-    console.log('userjs', req.file);
-    if(img === undefined) {
-        return res.status(400).send(util.fail(400, "이미지 없음"))
-    }
-    res.status(200).send(util.success(200, "sucess", img));
-  }
-}
+//   module.exports = router;
 
-module.export = User
+// const User = {
+//   uploadProfile: async (req, res) => {
+//     const img = req.file.path;
+//     console.log('userjs', req.file);
+//     if(img === undefined) {
+//         return res.status(400).send(util.fail(400, "이미지 없음"))
+//     }
+//     res.status(200).send(util.success(200, "sucess", img));
+//   }
+// }
+
+// module.export = User
