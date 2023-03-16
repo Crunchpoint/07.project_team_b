509bb3265ef15e8a49fe5173d36439425e5193d2
Crunchpoint <87291392+Crunchpoint@users.noreply.github.com>
2023-03-16 15:08:57 +0900

    feat: minor updates

--- components/social/Profile_contents.js
+++ components/social/Profile_contents.js
@@ -4,10 +4,13 @@ import styles from "@/styles/social/Profile_contents.module.scss";
 import Profile_post from "./Profile_post";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Swal from "sweetalert2";
+import { useSession } from "next-auth/react";
 
 const Profile_contents = () => {
   const { pageName, filteredBoard, postModal, setPostModal, setTextareaValue, currentUser } = useContext(MyContext);
   const router = useRouter();
+  const { data } = useSession();
 
   useEffect(() => {
     const handleClickOutside = (e) => {
@@ -23,6 +26,22 @@ const Profile_contents = () => {
     };
   }, [postModal]);
 
+  const handleButtonClick2 = () => {
+    Swal.fire({
+      title: "로그인이 필요해요",
+      width: 600,
+      padding: "2em",
+      color: "#716add",
+      background: "#fff url(/images/trees.png)",
+      backdrop: `
+        rgba(0,0,0,0.4)
+        url("/images/nyan-cat.gif")
+        left top
+        no-repeat
+      `,
+    });
+  };
+
   return (
     <>
       <div className={styles.contents}>
@@ -34,13 +53,19 @@ const Profile_contents = () => {
           <button className={styles.button} onClick={(e) => setPostModal(!postModal)}>
             Create New Post
           </button>
-        ) : (
+        ) : data?.user ? (
           <button className={styles.button}>
             <Link href={"/social/[user]"} as={`/social/${currentUser[0]?.user_name.toLowerCase()}`}>
               My Profile
             </Link>
           </button>
-        )}
+        ) : !data?.user ? (
+          <button className={styles.button}>
+            <Link onClick={handleButtonClick2} href='/login/login'>
+              Log in
+            </Link>
+          </button>
+        ) : null}
       </div>
       {postModal && <Profile_post />}
     </>
